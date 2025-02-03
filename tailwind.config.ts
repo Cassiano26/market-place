78939bc348ddb04ba052d95efc1e7b78d60513c9
Cassiano26 <candidocassiano2@gmail.com>
2025-02-03 13:58:54 -0300

    chore: add google fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
   content: [
@@ -8,6 +9,10 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-dm-sans)', ...fontFamily.sans],
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
