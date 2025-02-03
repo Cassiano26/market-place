a69c56f378575a9e04fdb9edb98ec74cb32cd498
Cassiano26 <candidocassiano2@gmail.com>
2025-02-03 15:04:34 -0300

    style: add auth layout

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Logo from '@/assets/Logo.svg'
+import BoxImage from '@/assets/Background.png'
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+
+  return (
+    <div className="grid grid-cols-[1.23fr,1fr] h-screen">
+      <div className="bg-background">
+       <div className='pt-10 pl-10'>
+        <Image src={Logo} alt='' width={267.36} height={68.65}/>
+       </div>
+       <div className=''>
+        <Image src={BoxImage} alt='' />
+       </div>
+      </div>
+      <div className="bg-background p-6">
+        {children}
+      </div>
+    </div>
+  );
+}
