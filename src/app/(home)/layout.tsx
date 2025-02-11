621af68e9c1e59e3329fb04894331262c8bdaa6c
Cassiano26 <candidocassiano2@gmail.com>
2025-02-11 13:55:46 -0300

    feat: add pages authorization

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,19 @@
-import { Header } from "@/components/Header";
+'use client'
+
+import { Header } from "@/components/Header"
+import { useRouter } from "next/navigation"
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
+  const emailCookie = document.cookie
+  .split("; ")
+  .find((cookie) => cookie.startsWith("market-place@email"))
+  ?.split("=")[1]
+
+  if(!emailCookie) {
+    router.push("/login")
+  }
 
   return (
     <div className='size-full flex justify-center pb-6'>
